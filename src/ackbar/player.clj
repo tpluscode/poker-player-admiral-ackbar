68ad8596090adbdb04c3f972b5f0572d16df2284
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2015-07-11 09:53:51 +0200

    Log game state

--- src/ackbar/player.clj
+++ src/ackbar/player.clj
@@ -1,9 +1,12 @@
-(ns ackbar.player)
+(ns ackbar.player
+  (:require
+   [taoensso.timbre :as log]))
 
 (def version "Admiral Ackbar lives!")
 
 (defn bet-request
   [game-state]
+  (log/info (pr-str game-state))
   0)
 
 (defn showdown
